2df6025f6b273528070c3dda31aee6934141df14
Snak3s <1289203637@qq.com>
2017-09-28 16:45:19 +0800

    Version 0.0.2.1

--- algo.h
+++ algo.h
@@ -1,6 +1,11 @@
 #include <cstdio>
 #include "rand.h"
 
+int _ufs_get(int u,int f[])
+{
+	return f[u]-u?f[u]=_ufs_get(f[u],f):u;
+}
+
 void rand_tree(int n)
 {
 	int f[n+5];
@@ -11,9 +16,9 @@ void rand_tree(int n)
 	{
 		int i=rand_int(1,n);
 		int j=rand_int(1,n);
-		if ((f[i]=f[f[i]])!=(f[j]=f[f[j]]))
+		if (_ufs_get(i,f)!=_ufs_get(j,f))
 		{
-			f[f[j]]=f[f[i]];
+			f[_ufs_get(i,f)]=j;
 			tot++;
 			printf("%d %d\n",i,j);
 		}
@@ -30,9 +35,9 @@ void rand_weighted_tree(int n,long long w1,long long w2)
 	{
 		int i=rand_int(1,n);
 		int j=rand_int(1,n);
-		if ((f[i]=f[f[i]])!=(f[j]=f[f[j]]))
+		if (_ufs_get(i,f)!=_ufs_get(j,f))
 		{
-			f[f[j]]=f[f[i]];
+			f[_ufs_get(i,f)]=j;
 			tot++;
 			printf("%d %d %lld\n",i,j,rand_ll(w1,w2));
 		}
